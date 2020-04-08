2029033d8221c140f2a3f38ad4f378d44613453f
Louis Korczowski <louis.korczowski@gmail.com>
2020-04-08 18:21:48 +0200

    added utils.config

---
+++ utils/config.py
@@ -0,0 +1,22 @@
+""" Configuration methods for tinnsleep loading/testing
+"""
+
+from glob import glob
+import os
+import getpass
+
+class Config():
+    """Load user-dependent variable and path
+    """
+
+    username = getpass.getuser()
+    known_users = {
+                    "louis": "/Users/louis/Data/SIOPI/",
+                    "runner": "/runner/"}
+
+    if username in known_users.keys():
+        data_path = known_users[username]
+    else:
+        raise KeyError(username + ": User path not defined, please add path in utils.config.")
+
+    bruxisme_files = sorted(glob(os.path.join(data_path, '*edf')))
