53d31203057837e1d62d1731daf38609acf3b476
Louis Korczowski <louis.korczowski@gmail.com>
2020-04-11 17:55:18 +0200

    huge and ugly commit

---
+++ transform/nlp.py
@@ -0,0 +1,23 @@
+import nltk
+from gensim.corpora import Dictionary, MmCorpus
+
+
+def nltk_stopwords():
+    nltk.download('stopwords')
+    return set(nltk.corpus.stopwords.words('french'))
+
+
+def prep_corpus(docs, additional_stopwords=set(), no_below=5, no_above=0.5):
+    print('Building dictionary...')
+    dictionary = Dictionary(docs)
+    stopwords = nltk_stopwords().union(additional_stopwords)
+    stopword_ids = map(dictionary.token2id.get, stopwords)
+    dictionary.filter_tokens(stopword_ids)
+    dictionary.compactify()
+    dictionary.filter_extremes(no_below=no_below, no_above=no_above, keep_n=None)
+    dictionary.compactify()
+
+    print('Building corpus...')
+    corpus = [dictionary.doc2bow(doc) for doc in docs]
+
+    return dictionary, corpus
--- transform/table.py
+++ transform/table.py
@@ -1,4 +1,49 @@
+import re
+import funcy as fp
+import pandas as pd
+
+
+EMAIL_REGEX = re.compile(r"[a-z0-9\.\+_-]+@[a-z0-9\._-]+\.[a-z]*")
+FILTER_REGEX = re.compile(r"[^a-z '#]")
+TOKEN_MAPPINGS = [(EMAIL_REGEX, "#email"), (FILTER_REGEX, ' ')]
 
 
 def map_column(df_table, dict_keywords=None):
     return df_table
+
+
+def tokenize_line(line):
+    res = line.lower()
+    for regexp, replacement in TOKEN_MAPPINGS:
+        res = regexp.sub(replacement, res)
+    return res.split()
+
+
+def tokenize(lines, token_size_filter=2):
+    tokens = fp.mapcat(tokenize_line, lines)
+    return [t for t in tokens if len(t) > token_size_filter]
+
+
+def load_doc(filename):
+    group, doc_id = filename.split('/')[-2:]
+    with open(filename, errors='ignore') as f:
+        doc = f.readlines()
+    return {'group': group,
+            'doc': doc,
+            'tokens': tokenize(doc),
+            'id': doc_id}
+
+
+def load_df(dfline):
+    group = dfline["etiologie"]
+    doc_id = dfline.name
+    courrier = dfline["courrier"]
+
+    if pd.isna(courrier):
+        doc = ""
+    else:
+        doc = dfline["courrier"].splitlines()
+    return {'group': group,
+            'doc': doc,
+            'tokens': tokenize(doc),
+            'id': doc_id}
