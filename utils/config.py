de2554fbd4ce8ba5a41d4da3c71008d210e00df2
Louis Korczowski <louis.korczowski@gmail.com>
2020-04-11 23:47:44 +0200

    new 15 model

--- utils/config.py
+++ utils/config.py
@@ -62,8 +62,10 @@ class Config():
      'vont', 'votre', 'vous', 'vu', 'ça',
      'étaient', 'état', 'étions', 'été', 'être']
     stop_words += ['qui','oui','vos','nos','merci','plus','docteur','cher','toujours','actuellement','salutations','avait','remercie']
-    stop_words +=['suis','avez','moi','bonjour','loche','vincent','dewaleyne','lettre']
+    stop_words += ['suis','avez','moi','bonjour','loche','vincent','dewaleyne','lettre']
 
 
     related_columns = {"etiologie": ["etiologie", "ETIOLOGIE FINALE"],
                        "epworth": ["epworth, epoworth 2"]}
+
+    dict_categories = {1}
